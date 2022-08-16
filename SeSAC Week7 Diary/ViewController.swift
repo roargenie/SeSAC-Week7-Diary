7d3886b25cf0448c41f777008e5ae5a9b1ee5f83
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-16 09:43:55 +0900

    Initial Commit

---
+++ SeSAC Week7 Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week7 Diary
+//
+//  Created by 이명진 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
